44685c47a2f898147239bbfc46cddaab87a282bf
Antonin Nhek <a.nhek@icloud.com>
2020-01-13 15:51:47 +0100

    added: tailwind colors + screens size.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
 module.exports = {
   theme: {
-    extend: {}
+    screens: {
+      'sm': '480px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1280px',
+    },
+    extend: {
+      colors: {
+        pink: '#db4068',
+        middlepink: '#e59795',
+        pastel: '#fbf0f0',
+        brown: '#362e31'
+      },
+    }
   },
   variants: {},
   plugins: []
