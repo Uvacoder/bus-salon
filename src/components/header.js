002a6b474c3e5f1005f35021bba9abe1dda4395c
Antonin Nhek <a.nhek@icloud.com>
2020-01-15 00:22:26 +0100

    updated: header

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,9 @@
-import PropTypes from "prop-types"
 import React from "react"
-import { ReactComponent as Instagram } from '../images/svg/instagram.svg'
 import { ReactComponent as Logo } from '../images/svg/camilia-logo.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import { FiFacebook, FiInstagram } from "react-icons/fi"
 
-const Header = ({ siteTitle }) => (
+const Header = () => (
   <header className="bg-white">
     <div className="px-6 mx-auto my-16 sm:px-8 max-w-7xl">
       <div className="flex flex-col justify-center md:flex-row md:justify-between">
@@ -24,23 +23,15 @@ const Header = ({ siteTitle }) => (
         </nav>
         <div className="flex justify-center mt-6 md:mt-0 md:items-center">
           <a href="http://instagram.com/camilia_coiffure/" className="flex items-center justify-center mr-2 rounded-full w-14 h-14 bg-pastel hover:bg-pink">
-            <Instagram className=""/>
+            <FiInstagram className="text-2xl text-brown" />
           </a>
           <a href="http://facebook.com/camiliacoiffure.lyon/" className="flex items-center justify-center rounded-full w-14 h-14 bg-pastel hover:bg-pink">
-            <Instagram className=""/>
+            <FiFacebook className="text-2xl text-brown" />
           </a>
         </div>
       </div>
     </div>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
