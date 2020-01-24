ebf00af8f4cdaa5dd31c5886f4ebec7c54a197fd
Antonin Nhek <a.nhek@icloud.com>
2020-01-24 21:01:50 +0100

    fixed: header on mobile.

--- src/components/header.js
+++ src/components/header.js
@@ -9,16 +9,18 @@ const Header = () => (
     <div className="px-6 mx-auto my-8 sm:my-10 sm:px-8 max-w-8xl">
       <div className="flex flex-col justify-center md:flex-row md:justify-between">
         <div className="flex-1">
-          <CamiliaLogo className="flex justify-center w-56 mx-auto md:mx-0" color1="#e73a65" color2="#4b3f42"/>
+          <div className="flex">
+            <CamiliaLogo className="flex justify-center w-56 mx-auto md:w-48 lg:w-56 md:mx-0" color1="#e73a65" color2="#4b3f42"/>
+          </div>
         </div>
-        <nav className="mt-8 text-center text-1xl font-branbold text-brown">
-          <AnchorLink href="#prestations" className="mr-6 lg:mr-8 hover:text-pink">
+        <nav className="flex justify-center text-center md:px-6 md:mt-0 md:items-center text-1xl font-branbold text-brown">
+          <AnchorLink href="#prestations" className="pt-2 mr-6 lg:mr-8 hover:text-pink">
             Prestations
           </AnchorLink>
-          <AnchorLink href="#salon" className="mr-6 lg:mr-8 hover:text-pink">
+          <AnchorLink href="#salon" className="pt-2 mr-6 lg:mr-8 hover:text-pink">
             Le salon
           </AnchorLink>
-          <AnchorLink href="#infos-pratiques" className="md:mr-6 lg:mr-8 hover:text-pink">
+          <AnchorLink href="#infos-pratiques" className="pt-2 hover:text-pink">
             Contact
           </AnchorLink>
         </nav>
