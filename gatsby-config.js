f98fec533e3c9460a11693f2b97b6878a6f027b6
Antonin Nhek <a.nhek@icloud.com>
2020-01-30 16:15:54 +0100

    added: PWA, offline config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     username: `@camiliacoiffure`,
   },
   plugins: [
+    `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-source-instagram`,
       options: {
@@ -43,6 +44,14 @@ module.exports = {
         theme_color: `#db4068`,
         display: `minimal-ui`,
         icon: `src/images/camilia-icon.png`, // This path is relative to the root of the site.
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/camilia-icon-512.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,        
       },
     },
     'gatsby-plugin-svgr',
@@ -62,6 +71,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
