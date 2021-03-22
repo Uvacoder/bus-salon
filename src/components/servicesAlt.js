35ef8c2c75405b9b98c4fc89393dff4fec6c3855
“anhek” <a.nhek@icloud.com>
2021-03-22 13:35:14 +0100

    fixed: prices pdf url on the right component.

--- src/components/servicesAlt.js
+++ src/components/servicesAlt.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react"
 import { FiCoffee, FiFileText, FiMessageCircle } from "react-icons/fi"
-import Icon24 from './icon24';
-import Container from './container';
-import AnchorLink from 'react-anchor-link-smooth-scroll';
+import Icon24 from "./icon24"
+import Container from "./container"
+import AnchorLink from "react-anchor-link-smooth-scroll"
 
 const ServicesAlt = () => {
   return (
@@ -10,52 +10,62 @@ const ServicesAlt = () => {
       <div className="flex flex-wrap justify-center">
         <div className="service-card">
           <Icon24>
-            <FiFileText className="text-brown"/>
+            <FiFileText className="text-brown" />
           </Icon24>
-          <h3 className="service-title">
-            Grille tarifaire des prestations
-          </h3>
+          <h3 className="service-title">Grille tarifaire des prestations</h3>
           <p className="mb-6">
-            Pour connaître le prix de nos différentes prestations (coupe, coloration, mèches, balayages, tie & dye, etc…)
+            Pour connaître le prix de nos différentes prestations (coupe,
+            coloration, mèches, balayages, tie & dye, etc…)
           </p>
-          <a href="https://www.dropbox.com/s/oh5wa8ktjr5pav8/camilia-tarifs-2020.pdf?dl=0" title="Télécharger le PDF des tarifs" className="service-link">
+          <a
+            href="https://www.dropbox.com/s/2wqlerqdo12ryyg/camilia-coiffure-tarifs.pdf?dl=0"
+            title="Télécharger le PDF des tarifs"
+            className="service-link"
+          >
             Télécharger le PDF
           </a>
         </div>
 
         <div className="service-card">
           <Icon24>
-            <FiCoffee className="text-brown"/>
+            <FiCoffee className="text-brown" />
           </Icon24>
           <h3 className="service-title">
             Le salon, un espace accueillant et cozy
           </h3>
           <p className="mb-6">
-            Marine, coiffeuse styliste & visagiste est à votre écoute pour vous conseiller et vous guider dans vos envies
+            Marine, coiffeuse styliste & visagiste est à votre écoute pour vous
+            conseiller et vous guider dans vos envies
           </p>
-          <AnchorLink href="#salon" title="Aller vers la section Salon" className="service-link">
+          <AnchorLink
+            href="#salon"
+            title="Aller vers la section Salon"
+            className="service-link"
+          >
             Découvrir le salon
           </AnchorLink>
         </div>
 
         <div className="service-card">
           <Icon24>
-            <FiMessageCircle className="text-brown"/>
+            <FiMessageCircle className="text-brown" />
           </Icon24>
-          <h3 className="service-title">
-            Contact et informations
-          </h3>
+          <h3 className="service-title">Contact et informations</h3>
           <p className="mb-6">
-            Besoin d’un renseignement pour une prestation ? Nous vous répondrons dans les plus brefs délais.
+            Besoin d’un renseignement pour une prestation ? Nous vous répondrons
+            dans les plus brefs délais.
           </p>
-          <AnchorLink href="#contactus" title="Aller vers le formulaire de contact" className="service-link">
+          <AnchorLink
+            href="#contactus"
+            title="Aller vers le formulaire de contact"
+            className="service-link"
+          >
             Nous écrire
           </AnchorLink>
         </div>
-        
       </div>
     </Container>
-  );
+  )
 }
- 
-export default ServicesAlt;
+
+export default ServicesAlt
